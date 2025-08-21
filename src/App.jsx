436266afa3d5182d55f0fd34eaa8087dc268b1cf
muhibk22 <m.muhibk22@gmail.com>
2025-08-22 04:00:32 +0500

    feat: fetch gifs from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [player, setPlayer] = useState({
+    score: 0,
+    best: 0,
+    visited: [],
+  });
+
+  const [gifs, setGifs] = useState([]);
+  const API_KEY = import.meta.env.VITE_GIPHY_KEY;
+
+  const fetchGifs = async (query = "Elden Ring") => {
+    try {
+      const res = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=10`
+      );
+      const data = await res.json();
+      setGifs(data.data.map((gif) => gif.images.downsized_medium.url));
+    } catch (error) {
+      console.error("Error fetching GIFs:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGifs();
+  }, []);
+
+  const increaseScore = () => {
+    setPlayer((prev) => ({
+      ...prev,
+      score: prev.score + 1,
+    }));
+  };
+
+  const resetGame = () => {
+    setPlayer((prev) => ({
+      ...prev,
+      score: 0,
+      visited: [],
+    }));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <p>Score: {player.score}</p>
+      <p>Best: {player.best}</p>
+      <button onClick={increaseScore}>Increase</button>
+      <button onClick={resetGame}>Reset</button>
+
+      <hr />
+      <h3>Random GIF</h3>
+      {gifs ? (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gap: "10px",
+          }}
+        >
+          {gifs.map((url, i) => (
+            <img
+              key={i}
+              src={url}
+              alt="gif"
+              style={{ width: "100%", borderRadius: "8px" }}
+            />
+          ))}
+        </div>
+      ) : (
+        <p>Loading....</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
